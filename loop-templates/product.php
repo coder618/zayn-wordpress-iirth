d464157ebb423aabc7384ab632ee9f9d0444feb8
Ahadul Islam <iamahadul@gmail.com>
2026-05-14 19:56:53 +0600

    switching fix

--- loop-templates/product.php
+++ loop-templates/product.php
@@ -23,12 +23,25 @@ if ( ! is_a( $product, 'WC_Product' ) ) {
     href="<?php echo esc_url( $product->get_permalink() ); ?>">
 
 
-    <div class="img-wrapper">
-
+    <div class="img-wrapper relative">
         <?php 
+            $gallery_image_ids = $product->get_gallery_image_ids();
+            $has_hover_image   = ! empty( $gallery_image_ids );
+            $main_img_class    = 'product-img';
+            
+            if ( $has_hover_image ) {
+                $main_img_class .= ' has-hover';
+            }
+
             echo $product->get_image( 'large', array(
-                'class' => 'product-img'
+                'class' => $main_img_class
             ) ); 
+
+            if ( $has_hover_image ) {
+                echo wp_get_attachment_image( $gallery_image_ids[0], 'large', false, array(
+                    'class' => 'product-img-hover'
+                ) );
+            }
         ?>
     </div>
     <h4><?php echo esc_html( $product->get_title() ); ?></h4>
