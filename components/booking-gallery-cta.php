69792a21e710efc0ebd94709d476b34ba0305b2e
Ahadul Islam <iamahadul@gmail.com>
2026-05-14 18:48:33 +0600

    before experiment cart pag banner

--- components/booking-gallery-cta.php
+++ components/booking-gallery-cta.php
@@ -19,45 +19,37 @@ $bg_url = $bg_image ? wp_get_attachment_url($bg_image) : '';
 $arrow_svg = '<svg xmlns="http://www.w3.org/2000/svg" width="12" height="12" fill="none" viewBox="0 0 12 12"><path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="1.33" d="M2.5 6h7M6 2.5 9.5 6 6 9.5"/></svg>';
 ?>
 
-<section class="booking-gallery-cta" style="--bg-image: url('<?php echo esc_url($bg_url); ?>');">
-    <div class="bgc-background"></div>
+<section class="booking-gallery-cta">
+    <div class="bgc-background " style="--bg-image: url('<?php echo esc_url($bg_url); ?>');"></div>
     <div class="container">
-        <div class="bgc-wrapper">
-            <div class="bgc-content">
-                <?php if ( $title ) : ?>
-                    <h2 class="bgc-title"><?php echo esc_html( $title ); ?></h2>
-                <?php endif; ?>
+        <div class="left-text-part">
+            <?php if ( $title ) : ?>
+            <h2 class="bgc-title"><?php echo esc_html( $title ); ?></h2>
+            <?php endif; ?>
 
-                <?php if ( $btn_title ) : ?>
-                    <a href="<?php echo esc_url( $btn_link ); ?>" class="bgc-btn">
-                        <?php echo esc_html( $btn_title ); ?>
-                        <span class="icon"><?php echo $arrow_svg; ?></span>
-                    </a>
-                <?php endif; ?>
+            <?php if ( $btn_title ) : ?>
+            <div class="btn-wrapper">
+                <a href="<?php echo esc_url( $btn_link ); ?>" class="shop-btn"><?php echo esc_html( $btn_title ); ?>
+                    <span><?php echo $arrow_svg; ?></span></a>
             </div>
 
-            <div class="bgc-gallery">
-                <?php if ( $gallery_1 ) : ?>
-                    <div class="bgc-img bgc-img-1">
-                        <?php echo wp_get_attachment_image( $gallery_1, 'large' ); ?>
-                    </div>
-                <?php endif; ?>
-                <?php if ( $gallery_2 ) : ?>
-                    <div class="bgc-img bgc-img-2">
-                        <?php echo wp_get_attachment_image( $gallery_2, 'large' ); ?>
-                    </div>
-                <?php endif; ?>
-                <?php if ( $gallery_3 ) : ?>
-                    <div class="bgc-img bgc-img-3">
-                        <?php echo wp_get_attachment_image( $gallery_3, 'large' ); ?>
-                    </div>
-                <?php endif; ?>
-                <?php if ( $gallery_4 ) : ?>
-                    <div class="bgc-img bgc-img-4">
-                        <?php echo wp_get_attachment_image( $gallery_4, 'large' ); ?>
-                    </div>
-                <?php endif; ?>
-            </div>
+            <?php endif; ?>
         </div>
+
+        <div class="right-image-part">
+            <?php if ( $gallery_1 ) : ?>
+            <?php echo wp_get_attachment_image( $gallery_1, 'large', false, array( 'class' => 'bgc-img bgc-img-1' ) ); ?>
+            <?php endif; ?>
+            <?php if ( $gallery_2 ) : ?>
+            <?php echo wp_get_attachment_image( $gallery_2, 'large', false, array( 'class' => 'bgc-img bgc-img-2' ) ); ?>
+            <?php endif; ?>
+            <?php if ( $gallery_3 ) : ?>
+            <?php echo wp_get_attachment_image( $gallery_3, 'large', false, array( 'class' => 'bgc-img bgc-img-3' ) ); ?>
+            <?php endif; ?>
+            <?php if ( $gallery_4 ) : ?>
+            <?php echo wp_get_attachment_image( $gallery_4, 'large', false, array( 'class' => 'bgc-img bgc-img-4' ) ); ?>
+            <?php endif; ?>
+        </div>
+
     </div>
 </section>
--- task.md
+++ task.md
@@ -1,12 +1,4 @@
 # To do
 
-1. cart and checkout page fixed
-2. Develop a home page component
-3. Tab version fix for home
-4. fix menu speacing in the desktop and tabs
-
-5. Mega menu link in shop page working check for category support
-6. Test the Jump link
-
-7. btn fix nav bar
-8. change pic when hover
+6. Fix single product page jump link Test the Jump link
+7. change pic when hover
